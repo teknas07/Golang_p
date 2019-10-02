6c846fa017f8204567c82b9861b96e64e930f875
teknas07 <sanketcbe@gmail.com>
2019-10-02 17:12:12 +0530

    go_operators

---
+++ operators.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Printf("Enter any two numbers:")
+	fmt.Scan(&x)
+	fmt.Scan(&y)
+	fmt.Printf("x + y = %d\n", x+y)
+	fmt.Printf("x - y = %d\n", x-y)
+	fmt.Printf("x * y = %d\n", x*y)
+	fmt.Printf("x / y = %d\n", x/y)
+	fmt.Printf("x mod y = %d\n", x%y)
+
+	x++
+	fmt.Printf("x++ = %d\n", x)
+
+	y--
+	fmt.Printf("y-- = %d\n", y)
+}
